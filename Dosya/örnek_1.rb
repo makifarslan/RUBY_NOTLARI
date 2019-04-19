0e46d7b151c9efd232c78d34da26d4c11361dcea
Mehmet Akif ARSLAN <37078543+makifarslan@users.noreply.github.com>
2019-04-19 16:42:57 +0300

    Add files via upload

---
+++ Dosya/örnek_1.rb
@@ -0,0 +1,31 @@
+class A
+  @@a1 = 20
+  @@a2 = 10
+  
+  def self.a1
+    @@a1
+  end
+  
+  class << self
+    attr_accessor :a2
+  end
+end
+  
+class B < A 
+  @@a1=60
+  @a2=30
+  
+  def self.a1
+    @@a1
+  end
+
+  class << self
+    attr_accessor :a2
+  end  
+end
+  
+p A.a1  
+p A.a2
+p B.a1 
+p B.a2
+# çıktılar   60  nil   60   30
